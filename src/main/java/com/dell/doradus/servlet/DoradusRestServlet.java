07755e9eb27d43dccacbbb4574a5784e5b4dc352
Tra Duong <tra.duong@software.dell.com>
2015-02-18 17:30:20 -0800

    build Doradus Servlet to work in Openshift

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -49,7 +49,9 @@ public class DoradusRestServlet extends RESTServlet {
 	@Override
 	public void init(ServletConfig config)  {
 
-		final String[] args = new String[] { "-dbhost", "10.228.23.117", "-dbport", "9042", "-dbuser", "SuperDory", "-dbpassword", "Alpha1"};
+		//final String[] args = new String[] { "-dbhost", "10.228.23.117", "-dbport", "9042", "-dbuser", "SuperDory", "-dbpassword", "Alpha1"};
+		final String[] args = new String[] { "-dbhost", "localhost", "-dbport", "9160"};
+
 		DoradusServer.startEmbedded(args, SERVICES);
 		System.out.println("doradus started... ");	
 	}
