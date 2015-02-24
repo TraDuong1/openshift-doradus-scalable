b8d060011445667da5a61c88154dd82219e5faa6
Tra Duong <tra.duong@software.dell.com>
2015-02-24 15:14:57 -0800

    build Doradus Servlet to work in Openshift

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -54,9 +54,7 @@ public class DoradusRestServlet extends RESTServlet {
     	System.out.println("DORADUS_PORT: " + System.getenv("DORADUS_PORT"));
        	System.out.println("DORADUS_DB_USER: " + System.getenv("DORADUS_DB_USER"));
       	System.out.println("DORADUS_DB_PASSWORD: " + System.getenv("DORADUS_DB_PASSWORD"));
-      	System.out.println("OPENSHIFT_LOG_DIR " + System.getProperty("$OPENSHIFT_LOG_DIR"));
-      	System.out.println("log4j.appender.file.File: " + System.getProperty("log4j.appender.file.File"));
-     	
+      	System.out.println("OPENSHIFT_LOG_DIR " + System.getenv("$OPENSHIFT_LOG_DIR"));
       	
 		final String[] args = new String[] { "-dbhost", System.getenv("DORADUS_HOST"), "-dbport", System.getenv("DORADUS_PORT"), "-dbuser", System.getenv("DORADUS_DB_USER"), "-dbpassword", System.getenv("DORADUS_DB_PASSWORD")};
 		//final String[] args = new String[] { "-dbhost", "10.228.23.117", "-dbport", "9042", "-dbuser", "SuperDory", "-dbpassword", "Alpha1"};
@@ -64,6 +62,8 @@ public class DoradusRestServlet extends RESTServlet {
 
 		DoradusServer.startEmbedded(args, SERVICES);
 		System.out.println("Doradus started... ");	
+		System.out.println("log4j.appender.file.File: " + System.getProperty("log4j.appender.file.File"));
+		
 	}
 	
 	private static final String[] SERVICES = new String[]{
