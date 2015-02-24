6d28730cca219227fd8b61e03cfa970ec9653d78
Tra Duong <tra.duong@software.dell.com>
2015-02-24 15:38:55 -0800

    build Doradus Servlet to work in Openshift

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -54,7 +54,9 @@ public class DoradusRestServlet extends RESTServlet {
     	System.out.println("DORADUS_PORT: " + System.getenv("DORADUS_PORT"));
        	System.out.println("DORADUS_DB_USER: " + System.getenv("DORADUS_DB_USER"));
       	System.out.println("DORADUS_DB_PASSWORD: " + System.getenv("DORADUS_DB_PASSWORD"));
-      	System.out.println("OPENSHIFT_LOG_DIR " + System.getenv("$OPENSHIFT_LOG_DIR"));
+      	System.out.println("OPENSHIFT_LOG_DIR " + System.getenv("OPENSHIFT_LOG_DIR"));
+      	System.out.println("doradus.log location " + System.getenv("OPENSHIFT_LOG_DIR")+"doradus.log");
+      	org.apache.log4j.PropertyConfigurator.configure(System.getenv("OPENSHIFT_LOG_DIR")+"doradus.log");
       	
 		final String[] args = new String[] { "-dbhost", System.getenv("DORADUS_HOST"), "-dbport", System.getenv("DORADUS_PORT"), "-dbuser", System.getenv("DORADUS_DB_USER"), "-dbpassword", System.getenv("DORADUS_DB_PASSWORD")};
 		//final String[] args = new String[] { "-dbhost", "10.228.23.117", "-dbport", "9042", "-dbuser", "SuperDory", "-dbpassword", "Alpha1"};
@@ -62,8 +64,6 @@ public class DoradusRestServlet extends RESTServlet {
 
 		DoradusServer.startEmbedded(args, SERVICES);
 		System.out.println("Doradus started... ");	
-		System.out.println("log4j.appender.file.File: " + System.getProperty("log4j.appender.file.File"));
-		
 	}
 	
 	private static final String[] SERVICES = new String[]{
