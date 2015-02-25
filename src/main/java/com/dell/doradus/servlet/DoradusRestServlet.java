b873a405384c79894548eb09c001ae4813935391
Tra Duong <tra.duong@software.dell.com>
2015-02-24 22:22:29 -0800

    build Doradus Servlet to work in Openshift

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -56,8 +56,7 @@ public class DoradusRestServlet extends RESTServlet {
     	System.out.println("DORADUS_PORT: " + System.getenv("DORADUS_PORT"));
        	System.out.println("DORADUS_DB_USER: " + System.getenv("DORADUS_DB_USER"));
       	System.out.println("DORADUS_DB_PASSWORD: " + System.getenv("DORADUS_DB_PASSWORD"));
-      	System.out.println("OPENSHIFT_LOG_DIR: " + System.getenv("OPENSHIFT_LOG_DIR"));
-      	
+      	System.out.println("OPENSHIFT_LOG_DIR: " + System.getenv("OPENSHIFT_LOG_DIR"));      	
       	System.out.println("doradus.log.dir:  " + System.getProperty("doradus.log.dir"));
     			
 		final String[] args = new String[] { "-dbhost", System.getenv("DORADUS_HOST"), "-dbport", System.getenv("DORADUS_PORT"), "-dbuser", System.getenv("DORADUS_DB_USER"), "-dbpassword", System.getenv("DORADUS_DB_PASSWORD")};
