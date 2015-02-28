f20174c23a0307f5c91d605a60e13c45ac10cfa0
Tra Duong <tra.duong@software.dell.com>
2015-02-27 19:44:38 -0800

    build Doradus Servlet to work in Openshift

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -1,42 +1,15 @@
 package com.dell.doradus.servlet;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.annotation.WebServlet;
 
-import com.dell.doradus.core.DoradusServer;
-import com.dell.doradus.service.olap.OLAPService;
 import com.dell.doradus.service.rest.RESTServlet;
-import com.dell.doradus.service.spider.SpiderService;
 
 /**
  * Servlet implementation class DoradusRestServlet
  */
 @WebServlet(name = "DoradusRestServlet", urlPatterns = {"/api/*"})
 public class DoradusRestServlet extends RESTServlet {
-	private static final long serialVersionUID = 1L;
-	private static final String[] SERVICES = new String[]{
-        SpiderService.class.getName(),
-        OLAPService.class.getName()
-    
-	};      
- 
-	@Override
-	public void init(ServletConfig config)  {
-		
-    	//System.out.println("DORADUS_HOST: " + System.getenv("DORADUS_HOST"));
-    	//System.out.println("DORADUS_PORT: " + System.getenv("DORADUS_PORT"));
-       	//System.out.println("DORADUS_DB_USER: " + System.getenv("DORADUS_DB_USER"));
-      	//System.out.println("DORADUS_DB_PASSWORD: " + System.getenv("DORADUS_DB_PASSWORD"));
-      	//System.out.println("OPENSHIFT_LOG_DIR: " + System.getenv("OPENSHIFT_LOG_DIR"));      	
-		//System.out.println("doradus.log.dir:  " + System.getProperty("doradus.log.dir"));   
-		
-		//final String[] args = new String[] { "-dbhost", "10.228.23.117", "-dbport", "9042", "-dbuser", "SuperDory", "-dbpassword", "Alpha1"};
-		//final String[] args = new String[] { "-dbhost", "localhost", "-dbport", "9160"};
-		
-		//start Doradus embedded server
-		final String[] args = new String[] { "-dbhost", System.getenv("CASSANDRA_NODE_IP"), "-dbport", System.getenv("CASSANDRA_NODE_PORT"), "-dbuser", System.getenv("CASSANDRA_SUPERUSER_NAME"), "-dbpassword", System.getenv("CASSANDRA_SUPERUSER_PW")};
-		DoradusServer.startEmbedded(args, SERVICES);
-	}
-	
+
+	private static final long serialVersionUID = 4815487822036229036L;
 
 }
---
+++ src/main/java/com/dell/doradus/servlet/DoradusServletContextListener.java
@@ -0,0 +1,39 @@
+package com.dell.doradus.servlet;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import com.dell.doradus.core.DoradusServer;
+import com.dell.doradus.service.olap.OLAPService;
+import com.dell.doradus.service.spider.SpiderService;
+
+/**
+ * Registering ServletContextListener
+ */
+@WebListener
+public class DoradusServletContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {  
+		final String[] args = new String[] { "-dbhost", "localhost", "-dbport", "9160"};	
+		
+		//start Doradus embedded server		
+		//final String[] args = new String[] { "-dbhost", System.getenv("CASSANDRA_NODE_IP"), "-dbport", System.getenv("CASSANDRA_NODE_PORT"), "-dbuser", System.getenv("CASSANDRA_SUPERUSER_NAME"), "-dbpassword", System.getenv("CASSANDRA_SUPERUSER_PW")};
+		
+		DoradusServer.startEmbedded(args, SERVICES);		
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static final String[] SERVICES = new String[]{
+        SpiderService.class.getName(),
+        OLAPService.class.getName()
+    
+	};      
+ 
+}
